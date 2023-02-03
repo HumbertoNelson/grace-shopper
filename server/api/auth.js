53c3f7a5670af33fab1d8e15b23b7ee72edde428
Gary White <glw494@gmail.com>
2023-02-03 18:00:52 -0500

    Added error messages and register route

--- server/api/auth.js
+++ server/api/auth.js
@@ -4,20 +4,39 @@ const { User } = require('../db');
 
 module.exports = app;
 
-app.post('/', async(req, res, next)=> {
-  try {
-    res.send(await User.authenticate(req.body));
-  }
-  catch(ex){
-    next(ex);
-  }
-});
-
 app.get('/', async(req, res, next)=> {
   try {
     res.send(await User.findByToken(req.headers.authorization));
   }
-  catch(ex){
-    next(ex);
+  catch (err){
+    res.status(500).json({
+      message: "Could not find user",
+      error: err.message,
+    });
+  }
+});
+
+app.post('/', async(req, res, next)=> {
+  try {
+    res.send(await User.authenticate(req.body));
+  }
+  catch (err){
+    res.status(500).json({
+      message: "Could not login user",
+      error: err.message,
+    });
+  }
+});
+
+app.post("/register", async (req, res) => {
+  try {
+    const token = await User.encryptUser(req.body);
+    res.json(token);
+  } 
+  catch (err) {
+    res.status(500).json({
+      message: "Could not register user",
+      error: err.message,
+    });
   }
 });
