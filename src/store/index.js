faa824226aca8ec2f85ed40446d4131b3eebd35a
Gary White <glw494@gmail.com>
2023-02-06 23:26:39 -0500

    Added Profile to reducer

--- src/store/index.js
+++ src/store/index.js
@@ -4,11 +4,13 @@ import logger from 'redux-logger';
 import auth from './auth';
 import cart from './cart';
 import orders from './orders';
+import profile from './profile';
 
 const reducer = combineReducers({
   auth,
   cart,
   orders,
+  profile,
 });
 
 const store = createStore(reducer, applyMiddleware(thunk, logger));
@@ -18,3 +20,4 @@ export default store;
 export * from './auth';
 export * from './cart';
 export * from './orders';
+export * from './profile';
