50440adadd8f71d046ce35d055f4ccd59edf136e
Gary White <glw494@gmail.com>
2023-02-07 21:52:38 -0500

    updateProfile thunk

--- src/store/profile.js
+++ src/store/profile.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const profile = (state = {}, action)=> {
-  if (action.type === 'SET_PROFILE') {
+  if (action.type === 'SET_PROFILE' || action.type === 'UPDATE_PROFILE') {
     return action.profile;
   }
   return state;
@@ -16,6 +16,18 @@ export const fetchProfile = () => {
       }
     });
     dispatch({ type: 'SET_PROFILE', profile: response.data });
+  };
+};
+
+export const updateProfile = (profile) => {
+  return async(dispatch) => {
+    const token = window.localStorage.getItem('token');
+    const response = await axios.put('/api/profile', profile, {
+      headers: {
+        authorization: token
+      }
+    });
+    dispatch({ type: 'UPDATE_PROFILE', profile: response.data });
   };
 };
 
