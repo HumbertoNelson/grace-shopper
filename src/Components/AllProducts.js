ff0ec41a3b4fa6f35e1b523e59772acdceddff56
Gary White <glw494@gmail.com>
2023-02-11 15:32:40 -0500

    Copied version from main branch

--- src/Components/AllProducts.js
+++ src/Components/AllProducts.js
@@ -1,38 +1,30 @@
-import React, {useState, useEffect} from 'react';
-import axios from 'axios';
-import { Link } from 'react-router-dom'
-
-
+import React, {useEffect} from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { addItem } from '../store';
+import { fetchProducts } from '../store';
 
 const Products = () => {
-    const [products, setProducts] = useState([]);
+  const { products } = useSelector(state => state);
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-        const fetchProducts = async () => {
-          const result = await axios('/api/products');
-          setProducts(result.data);
-        }
-        fetchProducts();
-      }, [])
-
-      return (
-        console.log(products),
-        <ul>
-          {products.map((product) => (
-            <li key = {product.id}>{product.name}
-              <p>Price: {product.price}</p>
-              <p>Weight: {product.weight}</p>
-              <p>Size: {product.size}</p>
-              <p>Color: {product.color}</p>
-              <img src={product.imageUrl}></img>
-            </li>
-          ))}
-        </ul>
-      );
-    }
+  useEffect(() => {
+    dispatch(fetchProducts());
+    }, [])
+    
+  return (
+    <ul>
+      {products.map((product) => (
+        <li key = {product.id}>{product.name}
+          <p>Price: {product.price}</p>
+          <p>Weight: {product.weight}</p>
+          <p>Size: {product.size}</p>
+          <p>Color: {product.color}</p>
+          <img src={product.imageUrl}></img>
+          <button onClick={() => dispatch(addItem(product))} id='Add Button'>Add To Cart</button>
+        </li>
+      ))}
+    </ul>
+  );
+}
 
 export default Products;
-
-
-
-
